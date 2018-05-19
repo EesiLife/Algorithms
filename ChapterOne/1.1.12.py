cbc97b9fb2873f3bcd6077248280751ee2940532
EesiLife <siyu3223@qq.com>
2018-05-19 15:00:45 +0800

    add 1.1.12.py

---
+++ ChapterOne/1.1.12.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/19 下午2:53
+# @Author  : EesiLife
+# @Site    : https://github.com/EesiLife/
+# @File    : 1.1.12.py
+# @Software: PyCharm
+
+if __name__ == '__main__':
+    a = [9-i for i in range(10)]
+    a = [a[a[i]] for i in range(10)]
+    print(a)
